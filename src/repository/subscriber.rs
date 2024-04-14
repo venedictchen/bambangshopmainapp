5c9e9be5a3e92d8889c6a3c1f8987f6a3b8372f2
venedict chen <venedictchen@gmail.com>
2024-04-15 02:48:39 +0700

    Implement add function in Subscriber repository

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -10,5 +10,14 @@ lazy_static! {
 pub struct SubscriberRepository;
 
 impl SubscriberRepository {
+    pub fn add(product_type: &str, subscriber: Subscriber) -> Subscriber {
+        let subscriber_value = subscriber.clone();
+        if SUBSCRIBERS.get(product_type).is_none() {
+            SUBSCRIBERS.insert(product_type.to_string(), DashMap::new());
+        }
 
+        SUBSCRIBERS.get(product_type).unwrap().
+            insert(subscriber.url.clone(), subscriber_value.clone());
+        return subscriber_value;
+    }
 }   
